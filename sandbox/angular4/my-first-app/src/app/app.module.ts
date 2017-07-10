35d76dc54a12ab98b68fe115fadb54243a866405
Maciej Kwiecien <maciej.kwiecien@gmail.com>
2017-07-10 22:32:22 +0200

    [Anuglar 4] assignment #1 (simple impl)

--- sandbox/angular4/my-first-app/src/app/app.module.ts
+++ sandbox/angular4/my-first-app/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent} from './success-alert/successAlert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule, FormsModule
---
+++ sandbox/angular4/my-first-app/src/app/success-alert/successAlert.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component(
+  {
+    selector: 'app-success-alert',
+    template: `<div class="alert-success">SUCCESS</div>`
+  }
+)
+export class SuccessAlertComponent {
+
+}
