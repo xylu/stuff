d9590c3662d32e9059ef5c6a68ef2e2dac14eb73
Maciej Kwiecien <maciej.kwiecien@gmail.com>
2017-07-10 22:11:24 +0200

    [Anuglar 4] components basics (selectors:element, attribute, class)

--- sandbox/angular4/my-first-app/src/app/app.component.ts
+++ sandbox/angular4/my-first-app/src/app/app.component.ts
@@ -1,7 +1,12 @@
 import {Component} from '@angular/core';
 
 @Component({
+  // ELEMENT selector:
   selector: 'app-root',
+  // ATTRIBUTE selector:
+  // selector: '[app-root]',
+  // CLASS selector
+  // selector: '.app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
