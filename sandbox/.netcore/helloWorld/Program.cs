ea5d10adcf57d6e993c33a3547f7144e14516667
Maciej Kwiecien <maciej.kwiecien@gmail.com>
2018-07-02 23:19:16 +0200

    [.NetCore] Hello world :-)

---
+++ sandbox/.netcore/helloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
