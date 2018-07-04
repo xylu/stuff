eff1b3c84b987a2764ebc35be92d4d2043aa59c0
Maciej Kwiecien <maciej.kwiecien@gmail.com>
2018-07-05 01:03:06 +0200

    [.NetCore] webApp - switched to proxy mode

--- sandbox/.netcore/webApp/Startup.cs
+++ sandbox/.netcore/webApp/Startup.cs
@@ -53,16 +53,18 @@ namespace webApp
                     template: "{controller}/{action=Index}/{id?}");
             });
 
+
             app.UseSpa(spa =>
             {
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
-
+                
                 spa.Options.SourcePath = "ClientApp";
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+                
                 }
             });
         }
